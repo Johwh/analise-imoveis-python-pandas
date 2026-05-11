799d26a1d4e2714cfdcd61501d57b2a75ba107a4
João Vitor <jovijulio12@gmail.com>
2026-05-10 23:00:55 -0300

    Create analise.py

---
+++ src/analise.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+print('Carregando base de dados...')
+
+df = pd.read_csv('data/aluguel.csv', sep=';')
+
+print('\nPrimeiras 5 linhas:')
+print(df.head())
+
+print('\nDimensões da base:')
+print(df.shape)
+
+print('\nInformações gerais:')
+print(df.info())
+
+print('\nValores nulos:')
+print(df.isnull().sum())
+
+print('\nIniciando limpeza dos dados...')
+
+df['Condominio'] = df['Condominio'].fillna(0)
+df['IPTU'] = df['IPTU'].fillna(0)
+
+df = df.dropna(subset=['Valor'])
+
+df = df.drop_duplicates()
+
+df = df[df['Valor'] > 0]
+df = df[df['Area'] > 0]
+
+print('\nDados após limpeza:')
+print(df.shape)
+
+print('\nTipos de imóveis:')
+print(df['Tipo'].value_counts())
+
+media_bairro = (
+    df.groupby('Bairro')['Valor']
+    .mean()
+    .sort_values(ascending=False)
+)
+
+print('\nTop 10 bairros com maior média de aluguel:')
+print(media_bairro.head(10))
+
+df.to_csv('data/aluguel_tratado.csv', sep=';', index=False)
+
+print('\nArquivo aluguel_tratado.csv gerado com sucesso.')
